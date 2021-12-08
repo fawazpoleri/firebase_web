7a510d9c89770b24c2c39bcad0e54d1d7b653222
fawaz <fawazpolery@gmail.com>
2021-12-08 20:04:58 +0530

    first commit

---
+++ firebaseConfig.js
@@ -0,0 +1,10 @@
+var config = {
+    apiKey: "AIzaSyBh_X5dQsXQXjMdGcC83tDghoTyiWSmRKc",
+ authDomain: "fir-web-f8f52.firebaseapp.com",
+ projectId: "fir-web-f8f52",
+ storageBucket: "fir-web-f8f52.appspot.com",
+ messagingSenderId: "333208922387",
+ appId: "1:333208922387:web:3f875fd04fc7080ad892e6"
+   };
+   firebase.initializeApp(config);
+//    var firestore = firebase.firestore()
