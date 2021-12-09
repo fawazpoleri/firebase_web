762fbf4ccca46e8bce89d3859b24eb3192b4582d
fawazpoleri <fawazpolery@gmail.com>
2021-12-09 11:42:39 +0530

    added

--- home.js
+++ home.js
@@ -1,10 +1,10 @@
-firebase.auth().onAuthStateChanged((user)=>{
-    if(!user){
-        location.replace("index.html")
-    }else{
-        document.getElementById("user").innerHTML = "Hello, "+user.email
-    }
-})
+// firebase.auth().onAuthStateChanged((user)=>{
+//     if(!user){
+//         location.replace("index.html")
+//     }else{
+//         document.getElementById("user").innerHTML = "Hello, "+user.email
+//     }
+// })
 
 
 //view firestore data
@@ -12,25 +12,7 @@ firebase.auth().onAuthStateChanged((user)=>{
 var firestore = firebase.firestore()
 
 
-function getAllDataOnce(){
-    db.collection("fomData").get().then((querySnapshot)=>{
-        var formdata = [];
-        querySnapshot.forEach(doc=>{
-            formdata.push(doc.data());
-        });
-    });
-    
-}
 
-function getAllDataRealtime(){
-    db.collection("fomData").onSnapshot((querySnapshot)=>{
-        var formdata = [];
-        querySnapshot.forEach(doc=>{
-            formdata.push(doc.data());
-        });
-    });
-    
-}
 
 
 
@@ -38,3 +20,7 @@ function getAllDataRealtime(){
 function logout(){
     firebase.auth().signOut()
 }
+
+
+
+//------------
